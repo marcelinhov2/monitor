b097facfded0c15bfa301ef1b416f778539e82d0
marcelinhov2 <marceloandrade150@gmail.com>
2016-10-19 15:04:23 -0200

    update

--- src/monitor.js
+++ src/monitor.js
@@ -1,5 +1,39 @@
 ;(function() {
-	'use strict';
+  'use strict';
 
-	
+  var Monitor = function(win) {
+    win = win || window;
+
+    console.log(moment);
+
+    var doc = win.document;
+    var timing = win.performance.timing;
+
+    function toSec(ms){
+      return ms / 1000;
+    }
+
+    function getStartRenderTime(){
+      return toSec(timing.domContentLoadedEventStart - timing.navigationStart);
+    }
+
+    function getServerTime(){
+
+    }
+
+    function getContentRequests(){
+      var requests = win.performance.getEntriesByType("resource");
+      console.log(requests);
+
+      return requests;
+    }
+
+    return {
+      getStartRenderTime: getStartRenderTime,
+      getContentRequests: getContentRequests
+    };
+
+  };
+
+  window.Monitor = new Monitor;
 })();
